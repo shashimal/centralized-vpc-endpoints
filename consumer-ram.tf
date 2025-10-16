466a9abf6a32a37f146c4840e00a858f5efed6ed
shashimal <shashimald@gmail.com>
2025-10-16 11:10:37 +0800

    Accepting TGW resource share in the consumer account

--- consumer-ram.tf
+++ consumer-ram.tf
@@ -1,7 +1,7 @@
 #Accepting the RAM TGW resource share in consumer account
 resource "aws_ram_resource_share_accepter" "tgw_accepter_consumer_account" {
-  provider   = "aws.consumer-account"
-  share_arn  = aws_ram_resource_share.central_tgw_share.arn
+  provider  = "aws.consumer-account"
+  share_arn = aws_ram_resource_share.central_tgw_share.arn
   depends_on = [
     aws_ram_principal_association.consumer_account_association
   ]
